c64bfb9bad4b7f201fc6e313f5a634cb4bc62d41
SeanWu <seanwu@yunhetong.net>
2016-12-29 18:41:05 +0800

    add updateUserInfo and modify token.php

--- example/updateUserInfo.php
+++ example/updateUserInfo.php
@@ -2,9 +2,10 @@
 /**
  * Created by PhpStorm.
  * User: Seanwu
- * Date: 2016/5/18
- * Time: 15:25
+ * Date: 2016/12/28
+ * Time: 17:25
  */
+
 include_once("../library/LxSDKManager.class.php");
 include_once("./resource/R.php");
 header('Content-type: application/json;charset=utf-8');
